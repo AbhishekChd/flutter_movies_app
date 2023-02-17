e7e2cb2194c3c831fcfd9182a3b6d4429eedd7ab
Abhishek Chaudhary <abhishek.chaudhary.in@gmail.com>
2023-02-18 04:55:13 +0530

    Random movie card generator

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_movies_app/common_widgets/common_widgets.dart';
 
@@ -16,14 +17,29 @@ class HomeScreen extends StatelessWidget {
           crossAxisSpacing: 4,
         ),
         itemCount: 10,
-        itemBuilder: (context, index) {
-          return const MovieCard(
-            name: "The Dark Knight",
-            genres: ["Adventure", "Fight"],
-            rating: 3.8,
-          );
-        },
+        itemBuilder: (context, index) => _getRandomMovieCard(),
       ),
     );
+  }
+
+  // todo: Remove random movie generator
+  MovieCard _getRandomMovieCard() {
+    const movies = [
+      MovieCard(name: "The Dark Knight", genres: ["Adventure", "Fight"], rating: 3.8),
+      MovieCard(
+        name: "We're the Millers",
+        genres: ["Comedy", "Crime"],
+        rating: 3.5,
+        imageUrl: "https://m.media-amazon.com/images/M/MV5BMjA5Njc0NDUxNV5BMl5BanBnXkFtZTcwMjYzNzU1OQ@@._V1_.jpg",
+      ),
+      MovieCard(
+        name: "Suits",
+        genres: ["Comedy", "Drama", "Legal Drama"],
+        rating: 4.25,
+        imageUrl: "https://movieposters2.com/images/1479906-b.jpg",
+      ),
+    ];
+    int index = Random().nextInt(3);
+    return movies[index];
   }
 }
