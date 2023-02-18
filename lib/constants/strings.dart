42da2ff23312a70ec963d85ef290bc1c97db4db4
Abhishek Chaudhary <abhishek.chaudhary.in@gmail.com>
2023-02-18 05:44:28 +0530

    Add basic Settings UI

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -5,4 +5,10 @@ abstract class Strings {
   static const String bottomNavFavourites = "Favorites";
   static const String bottomNavWatchlist = "Watchlist";
   static const String bottomNavProfile = "Profile";
+
+  static const String settingTitleGeneral = "General";
+  static const String settingTitleDarkMode = "Dark Mode";
+  static const String settingItemApiKey = "API Key";
+  static const String settingItemApiKeyDefault = "Tap to edit";
+  static const String settingItemThemeSystem = "Automatic (System default)";
 }
--- lib/ui/profile/profile_screen.dart
+++ lib/ui/profile/profile_screen.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_movies_app/constants/strings.dart';
+import 'package:settings_ui/settings_ui.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final _settingsMargin = const EdgeInsetsDirectional.all(8);
+  bool _systemTheme = true;
+  bool _darkMode = true;
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("ProfileScreen"));
+    return SettingsList(
+      sections: [
+        SettingsSection(
+          margin: _settingsMargin,
+          title: Text(Strings.settingTitleGeneral, style: Theme.of(context).textTheme.labelLarge),
+          tiles: <SettingsTile>[
+            SettingsTile.navigation(
+              leading: const Icon(Icons.key),
+              title: const Text(Strings.settingItemApiKey),
+              value: const Text(Strings.settingItemApiKeyDefault),
+              onPressed: (context) {},
+            ),
+          ],
+        ),
+        SettingsSection(
+          margin: _settingsMargin,
+          title: Text(Strings.settingTitleDarkMode, style: Theme.of(context).textTheme.labelLarge),
+          tiles: <SettingsTile>[
+            SettingsTile.switchTile(
+              leading: const Icon(Icons.settings_outlined),
+              title: const Text(Strings.settingItemThemeSystem),
+              initialValue: _systemTheme,
+              onToggle: (bool value) {
+                setState(() {
+                  _systemTheme = !_systemTheme;
+                });
+              },
+            ),
+            SettingsTile.switchTile(
+              leading: const Icon(Icons.dark_mode_outlined),
+              title: const Text(Strings.settingTitleDarkMode),
+              initialValue: _darkMode,
+              enabled: !_systemTheme,
+              onToggle: (bool value) {
+                setState(() {
+                  _darkMode = value;
+                });
+              },
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
